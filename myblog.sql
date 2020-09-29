ab0d9486735f77ef58e7e4fbe3e020c6deeb2321
sampalm <samuelpalmeira@outlook.com>
2020-09-28 21:36:31 -0300

    updates SQL script

--- myblog.sql
+++ myblog.sql
@@ -2,6 +2,7 @@ CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
 
