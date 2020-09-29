9fe4c6cc97082914ba603e91ca24f87fe7cfa186
sampalm <samuelpalmeira@outlook.com>
2020-09-28 21:08:07 -0300

    adds config file

---
+++ Boot/Config.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * DATABASE
+ */
+define("DATA_LAYER_CONFIG", [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "port" => "3306",
+    "dbname" => "api",
+    "username" => "root",
+    "passwd" => "",
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
+
+/**
+ * PROJECT URLs
+ */
+define("URL_BASE", "http://localhost/RestApi");
+
+/**
+ * DATES
+ */
+define("CONF_DATE_BR", "d/m/Y H:i:s");
+define("CONF_DATE_APP", "Y-m-d H:i:s");
+
