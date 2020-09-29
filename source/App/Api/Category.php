1a1dba0f75a6dac507165d3ac1d94cd447f4f5a5
sampalm <samuelpalmeira@outlook.com>
2020-09-28 21:37:12 -0300

    adds category crud functions

--- source/App/Api/Category.php
+++ source/App/Api/Category.php
@@ -4,11 +4,95 @@ namespace Source\App\Api;
 
 use Core\Controller;
 
-class Category extends Controller {
-    public function index() 
+class Category extends Controller
+{
+    public function index()
     {
-        // "title = :title AND name = :name", "title={$title}&name={$name}"
-        $cat = (new \Source\Models\Category())->find()->fetch(true);
-        var_dump($cat);
+        header("Content-type: application/json; charset=utf-8");
+
+        $cats = (new \Source\Models\Category())->find()->fetch(true);
+
+        if ($cats) {
+            $response = array();
+
+            $i = 0;
+            foreach ($cats as $cat) {
+                $i++;
+                $response[$i] = $cat->data();
+            }
+
+            echo json_encode($response);
+            return;
+        }
+
+        echo $this->message->response("Nenhum artigo encontrado", 404)->json();
+    }
+
+    public function create(?array $data)
+    {
+        header("Content-type: application/json; charset=utf-8");
+
+        if (!$data) {
+            echo $this->message->response("Dados invalidos", 400)->json();
+            return;
+        }
+
+        $cat = new \Source\Models\Category();
+        $cat->name = $data["name"] ?? "";
+
+        if ($cat->save()) {
+            echo $this->message->response("Categoria salva com sucesso", 201)->json();
+            return;
+        }
+
+        echo $cat->message()->json();
+    }
+
+    public function update(?array $data)
+    {
+        header("Content-type: application/json; charset=utf-8");
+
+        if (!$data) {
+            echo $this->message->response("Dados invalidos", 400)->json();
+            return;
+        }
+
+        $cat = (new \Source\Models\Category())->findById($data["id"]);
+        if (empty($cat)) {
+            echo $this->message->response("Categoria não encontrado", 404)->json();
+            return;
+        }
+
+        $cat->name = $data["name"];
+
+        if ($cat->save()) {
+            echo $this->message->response("Categoria atualizada com sucesso", 200)->json();
+            return;
+        }
+
+        echo $cat->message()->json();
+    }
+
+    public function delete(?array $data)
+    {
+        header("Content-type: application/json; charset=utf-8");
+
+        if (!$data) {
+            echo $this->message->response("Dados invalidos", 400)->json();
+            return;
+        }
+
+        $cat = (new \Source\Models\Category())->findById($data["id"]);
+        if (empty($cat)) {
+            echo $this->message->response("Categoria não encontrado", 404)->json();
+            return;
+        }
+
+        if ($cat->destroy()) {
+            echo $this->message->response("Categoria removida com sucesso", 200)->json();
+            return;
+        }
+
+        echo $cat->message()->json();
     }
 }
--- source/Models/Category.php
+++ source/Models/Category.php
@@ -11,4 +11,34 @@ class Category extends Model {
         parent::__construct("categories", []);
     }
 
+    public function save(): bool {
+        if (strlen($this->name) < 5) {
+            $this->message->response("Erro título do post muito pequeno.", 400);
+            return false;
+        }
+
+        /** Update */
+        if (!empty($this->id)) {
+            $Id = $this->id;
+
+            $this->update($this->safe(), "id = :id", "id={$Id}");
+            if ($this->fail()) {
+                $this->message->response("Erro ao atualizar, verifique os dados", 400);
+                return false;
+            }
+        }
+
+        /** Create */
+        if (empty($this->id)) {
+            $Id = $this->create($this->safe());
+            if ($this->fail()) {
+                $this->message->response("Erro ao cadastrar, verifique os dados", 400);
+                return false;
+            }
+        }
+
+        $this->data = ($this->findById($Id))->data();
+        return true;
+    }
+
 }
